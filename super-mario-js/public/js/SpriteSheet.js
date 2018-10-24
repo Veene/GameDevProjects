f864d9695cec3366966f9780a4f6c34d73abc46e
John Pawlak <johnpawlak@gmail.com>
2018-10-23 21:10:11 -0400

    refactor files

---
+++ super-mario-js/public/js/SpriteSheet.js
@@ -0,0 +1,32 @@
+//we created this class prototype, to avoid using the crazy 8 input context.drawImage(x1,x2,x3,x4,x5,x6,x7,x8)
+//invoke with simple new SpriteSheet(image, width, height)
+export default class SpriteSheet {
+    constructor(image, width, height) {
+        this.image = image;
+        this.width = width;
+        this.height = height;
+        this.tiles = new Map(); //using map to hold key,value pairs, in this case: name of tile and buffer.
+    }
+    define(name, x, y) { //name the tile and where starting point is (topleft corner)
+        const buffer = document.createElement('canvas');
+        buffer.width = this.width; //automatically tells you height and width which was invoke with new Spritesheet
+        buffer.height = this.height;
+        buffer
+            .getContext('2d')
+            .drawImage(
+                this.image, 
+                x * this.width, //what column you want the item (x=0,1,2,3,4)
+                y * this.height, //what row you want the item (y=0, 1, 2, 3, 4)
+                this.width, 
+                this.height, 
+                0, 
+                0, 
+                this.width, 
+                this.height);
+        this.tiles.set(name, buffer); //set key value pair
+    }
+    draw(name, context, x, y) {
+        const buffer = this.tiles.get(name); //using the map that is storing k:v's and getting buffer.
+        context.drawImage(buffer, x, y);
+    }
+}
--- super-mario-js/public/js/main.js
+++ super-mario-js/public/js/main.js
@@ -1,4 +1,28 @@
+import SpriteSheet from './SpriteSheet.js';
+import {loadImage} from './loaders.js';
+
+function loadImage(url) {
+    return new Promise(resolve => {
+        const image = new Image();
+        image.addEventListener('load', () => {
+            resolve(image);
+        });
+        image.src = url;
+    });
+}
+
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 context.fillRect(0, 0, 50, 50, 'black')
+
+loadImage('/img/tiles.png')
+.then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('ground', 0, 0);
+    sprites.draw('ground', context, 5, 20)
+    // context.drawImage(image, 
+    //     16, 0, 16, 16, // how much are we grabbing? 0,0 TO 16x 16y creates a 16x16 pixel from top left
+    //     32, 32, 16, 16); // x1,x2 = top left corner position of that piece that we ripped off tiles, so here moved 32x and 32y, x3,x4 = scaling, since we ripped 16x16, it should be 16x16 but u can scale higher
+});
